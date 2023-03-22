0e8769755f4a4a72a69505e7adae40fe1264147d
unknown-bytes <unknown.develops@gmail.com>
2023-03-22 02:41:02 -0400

    ...

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-
-
+app.get("/", (req,res) => {
+    res.send("Default Route.")
+})
+app.get("/greetings/:name", (req,res) => {
+    const name = req.params.name;
+    res.send(`Hi ${name}`)
+})
 
  app.listen(process.env.PORT, () => {
     console.log(`Listening on PORT:${process.env.PORT}👂`)

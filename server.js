3a2d30826e11271c6b717b19520a3a62c4267f12
unknown-bytes <unknown.develops@gmail.com>
2023-03-22 03:12:00 -0400

    added tip calc

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.get("/greetings/:name", (req,res) => {
     const name = req.params.name;
     res.send(`Hi ${name}`)
 })
+app.get("/tip/:bill/:percentage", (req,res) => {
+    const percentage = req.params.percentage;
+    const whole = req.params.bill;
+    let tip = (percentage * whole) / 100;
+    res.send(`Tip: ${tip}`)
+})
 
  app.listen(process.env.PORT, () => {
     console.log(`Listening on PORT:${process.env.PORT}👂`)

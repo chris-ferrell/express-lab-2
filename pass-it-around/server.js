2eb731f9f32392871e4f46b7cc2e50c160181da9
unknown-bytes <unknown.develops@gmail.com>
2023-03-23 13:48:46 -0400

    Beer on the wall

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -1,17 +1,36 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-let bottles = 99;
-app.get("/", (req,res) => {
-    res.send("<h1>99 Bottles of beer on the wall</h1> \n <body><a href='/98'> take one down, pass it around</a></body>");
-   
-})
+ 
+// Set up a global variable to store the current number of bottles
+let numBottles = 99;
 
-app.listen(3000, () => {
-    console.log("pass it around Listening");
-})
+// Define a route for the home page
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>${numBottles} Bottles of beer on the wall</h1>
+    <a href="/${numBottles - 1}">take one down, pass it around</a>
+  `);
+});
 
+// Define a route that takes a number of bottles as a parameter
+app.get('/:number_of_bottles', (req, res) => {
+  const { number_of_bottles } = req.params;
 
-app.get("/98", (req,res) => {
-    res.send("1")
-})
+  if (number_of_bottles == 0) {
+    
+    res.send(`
+      <h1>No more bottles of beer on the wall</h1>
+      <a href="/">start over</a>
+    `);
+  } else {
+    res.send(`
+      <h1>${number_of_bottles} Bottles of beer on the wall</h1>
+      <a href="/${number_of_bottles - 1}">take one down, pass it around</a>
+      <br>
+      
+    `);
+  }
+});
 
+// Start the server
+app.listen(3000, () => console.log(`Listening on 🅿️ 🍩R🌵:3000 📶`));

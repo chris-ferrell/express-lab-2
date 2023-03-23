02a19ea8c53fdd0d87fa95dd07f42aebc6d43cda
unknown-bytes <unknown.develops@gmail.com>
2023-03-22 22:31:00 -0400

    added redirect and 98 route

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
+let bottles = 99;
 app.get("/", (req,res) => {
-    res.send("99 Bottles of beer on the wall");
+    res.send("<h1>99 Bottles of beer on the wall</h1> \n <body><a href='/98'> take one down, pass it around</a></body>");
+   
 })
 
 app.listen(3000, () => {
     console.log("pass it around Listening");
 })
 
+
+app.get("/98", (req,res) => {
+    res.send("1")
+})
 
